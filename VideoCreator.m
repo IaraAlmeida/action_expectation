4e95e5f880deeba64408a0669a5e14dd07235eab
ghila93 <38372956+ghila93@users.noreply.github.com>
2020-11-11 15:22:51 +0100

    Update VideoCreator.m

--- VideoCreator.m
+++ VideoCreator.m
@@ -2,36 +2,30 @@
 %                        Video creation                             %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 infoVar;
+file  = 'DATA//sub2.oxy3';
 
 %% reading file
 cfg = [];
-cfg.dataset = 'DATA//sub2.oxy3';
-cfg.demean  = 'yes';
+cfg.dataset = file;
 [data] = ft_preprocessing(cfg);
 
 cfg = [];
-cfg.channel = ft_channelselection('R*', data.label);
-NIRS = ft_selectdata(cfg, data);
-dataP=NIRS;
-%% ADD accelerometer
-cfg = [];
-cfg.channel = ft_channelselection({'A*','-ADC001','-ADC002','-ADC003','-ADC004', '-ADC008'}, data.label);
+cfg.channel = ft_channelselection({'A*'}, data.label);
 Acc = ft_selectdata(cfg, data);
 
-load('DATA//S_Npre_02.mat')
-Acc.trial{1}= nirs_data.ADvalues(:,[5 6 7]).';
 
 %% preprocessing
-% cfg             = [];
-% cfg.dpf         = 5; %redo for proper dpf 
-% dataP           = ft_nirs_transform_ODs(cfg, NIRS); % IN MICROMOLAR
+cfg             = [];
+cfg.dpf         = 5; 
+dataP           = ft_nirs_transform_ODs(cfg, data); % IN MICROMOLAR
 
 cfg = [];
 data_merged =  ft_appenddata(cfg, dataP, Acc);
 
+
 %% Find events
 cfg                                   = [];
-cfg.dataset                           = 'DATA//sub2.oxy3';
+cfg.dataset                           = filen;
 cfg.trialdef.eventtype                = 'event';
 [EVENTS]                              = ft_definetrial(cfg);
 
@@ -41,6 +35,37 @@ EVENTS.event = EVENTS.event(eliminate);
 
 eliminate2 = ismember({EVENTS.event.value}, {'LSL 1','LSL 2','LSL '});
 EVENTS.event = EVENTS.event(~eliminate2);
+
+
+%% try settings out
+cfg = [];
+cfg.toilim              = [150 data_merged.time{1}(end)];
+cfg.trackcallinfo       = 'no';
+cfg.showcallinfo        = 'no';
+data_piece              = ft_redefinetrial(cfg, data_merged);
+
+cfg = [];
+cfg.viewmode            = 'vertical';
+cfg.event               = EVENTS.event;
+cfg.ploteventlabels     = 'colorvalue';
+cfg.plotlabels          = 'yes';
+cfg.fontsize            = 6.1;
+cfg.continuous          = 'yes';
+cfg.blocksize           = 15;
+cfg.nirsscale           = 6000;
+cfg.mychan              = {'ADC005','ADC006','ADC007'};
+cfg.mychanscale         = [5,5,5];
+cfg.ylim = 'maxmin';
+cfg.preproc.demean      = 'yes';
+cfg.linecolor=[1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1;
+    1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1;
+    1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1;
+    1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1;
+    1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1;0,0,0;0,1,0;0.9290, 0.6940, 0.1250];
+cfg.linewidth           = 1.16;
+cfg.verticalpadding     =0.09;
+
+ft_databrowser(cfg, data_piece); 
 
 
 %% Video creation
@@ -103,48 +128,6 @@ while (endtime < data_merged.time{1}(end)) && ishandle(figh)
 end
 % Close the file
 close(vidObj);
-
-
-
-%% try scale
-cfg = [];
-cfg.toilim              = [150 data_merged.time{1}(end)];
-cfg.trackcallinfo       = 'no';
-cfg.showcallinfo        = 'no';
-data_piece              = ft_redefinetrial(cfg, data_merged);
-
-cfg = [];
-cfg.viewmode            = 'vertical';
-cfg.event               = EVENTS.event;
-cfg.ploteventlabels     = 'colorvalue';
-cfg.plotlabels          = 'yes';
-cfg.fontsize            = 6.1;
-cfg.continuous          = 'yes';
-cfg.blocksize           = 15;
-cfg.nirsscale           = 6000;
-cfg.mychan              = {'ADC005','ADC006','ADC007'};
-cfg.mychanscale         = [5,5,5];
-cfg.ylim = 'maxmin';
-cfg.preproc.demean      = 'yes';
-cfg.linecolor=[1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1;
-    1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1;
-    1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1;
-    1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1;
-    1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1; 1, 0, 0;0, 0, 1;0,0,0;0,1,0;0.9290, 0.6940, 0.1250];
-cfg.linewidth           = 1.16;
-cfg.verticalpadding     =0.09;
-
-ft_databrowser(cfg, data_piece); 
-% set(gca, 'XGrid', 'on');
-
-
-%% try acc
-cfg.blocksize           = 60;
-cfg.viewmode            = 'vertical';
-cfg.mychan              = {'ADC005','ADC006','ADC007'};
-% cfg.mychanscale         = [0.2,0.5,1];
-cfg.linecolor = [0,0,0;0,1,0;0.9290, 0.6940, 0.1250];
-ft_databrowser(cfg, Acc); 
 
 
 
